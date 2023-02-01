57f70bb10b859e38608669c5f123dab4c8ce9701
nicu <lino479Nick@mail.ru>
2023-02-01 15:53:15 +0300

    Changed to a Browser Router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </HashRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
